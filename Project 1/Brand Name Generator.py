fce12836a5be1d81b6a165dd2e061fa68e6a4284
gavksingh <ksingh.gav@gmail.com>
2023-08-20 20:28:21 +0530

    fist project

---
+++ Project 1/Brand Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator!")
+city= input("What is name of the city you grow up in? \n")
+pet= input("What is name of your pet?\n")
+print("Your band name is: " + city + " " + pet)
